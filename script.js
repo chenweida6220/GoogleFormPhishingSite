17984721d53c611c886546bcf22b2f8446d5875d
chenweida6220 <weida.wdc@gmail.com>
2022-08-12 15:24:22 -0400

    both buttons now play audio and show image

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-function show() {
+function exit_and_play() {
     /* Access image by id and change the display property to block*/
-    document.getElementById('gif_rickroll')
+    document.getElementById('gif_screamingbeaver')
             .style.display = "block";
+
+    document.getElementById('audio_screamingbeaver').play()
 
     document.getElementById('buttonSubmit')
             .style.display = "none";
@@ -9,3 +11,20 @@ function show() {
     document.getElementById('buttonExit')
             .style.display = "none";
 }
+
+function submit_and_play() {
+    /* Access image by id and change the display property to block*/
+    document.getElementById('gif_rickroll')
+            .style.display = "block";
+
+    document.getElementById('audio_rickroll').play()
+}
+
+// function loading() {
+//     console.log($('form')[0].checkValidity())
+//     //use on form
+//     if ($('form')[0].checkValidity()) {
+//       $("#loading").show();
+//       $("#content").hide();
+//     }
+//   }
